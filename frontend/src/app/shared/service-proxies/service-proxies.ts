fd20283e3fac64947fbd3621e34e8c2c09512df9
wangguanglei <wangguanglei@wisecotech.com>
2024-03-29 18:13:24 +0800

    fx: xx

--- frontend/src/app/shared/service-proxies/service-proxies.ts
+++ frontend/src/app/shared/service-proxies/service-proxies.ts
@@ -862,6 +862,7 @@ export class StatisticsServiceProxy {
 
         let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
         if (status === 200) {
+            
             return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
             let result200: any = null;
             let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
